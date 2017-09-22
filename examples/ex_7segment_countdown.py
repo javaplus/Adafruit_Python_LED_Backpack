cf1b9cd2dba368f5cc79b1b601b6e3996dae7879
Barry Tarlton <TARLTOB1@nationwide.com>
2017-09-21 23:02:33 -0400

    re-indented everything

--- examples/ex_7segment_countdown.py
+++ examples/ex_7segment_countdown.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
 import time
-import datetime
 
 from Adafruit_LED_Backpack import SevenSegment
 
@@ -16,39 +15,40 @@ segment.begin()
 print "Press CTRL+Z to exit"
 
 def clearDisplay():
-   segment.clear()
-  # Set hours
-  segment.set_digit(0, ' ')
-  segment.set_digit(1, ' ')         
-  # Set minutes
-  segment.set_digit(2, ' ')
-  segment.set_digit(3, ' ')
-  
+    segment.clear()
+    # Set hours
+    segment.set_digit(0, " ")
+    segment.set_digit(1, " ")
+    # Set minutes
+    segment.set_digit(2, " ")
+    segment.set_digit(3, " ")
+
+
 # Continually update the time on a 4 char, 7-segment display
 counter = 9999
 while(counter>999):
   
-  hour = int(counter/100)
-  minute = int(counter % 100)
+    hour = int(counter/100)
+    minute = int(counter % 100)
 
 
-  segment.clear()
-  # Set hours
-  segment.set_digit(0, int(hour / 10))     # Tens
-  segment.set_digit(1, hour % 10)          # Ones
-  # Set minutes
-  segment.set_digit(2, int(minute / 10))   # Tens
-  segment.set_digit(3, minute % 10)        # Ones
-  # Toggle colon
-  #segment.set_colon(counter % 2)              # Toggle colon at 1Hz
+    segment.clear()
+    # Set hours
+    segment.set_digit(0, int(hour / 10))     # Tens
+    segment.set_digit(1, hour % 10)          # Ones
+    # Set minutes
+    segment.set_digit(2, int(minute / 10))   # Tens
+    segment.set_digit(3, minute % 10)        # Ones
+    # Toggle colon
+    #segment.set_colon(counter % 2)              # Toggle colon at 1Hz
 
-  # Write the display buffer to the hardware.  This must be called to
-  # update the actual display LEDs.
-  segment.write_display()
+    # Write the display buffer to the hardware.  This must be called to
+    # update the actual display LEDs.
+    segment.write_display()
 
-  # Wait a quarter second (less than 1 second to prevent colon blinking getting$
-  time.sleep(0.15)
-  counter=counter-9
+    # Wait a quarter second (less than 1 second to prevent colon blinking getting$
+    time.sleep(0.15)
+    counter=counter-9
 
 clearDisplay()
 
